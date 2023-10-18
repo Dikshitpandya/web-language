a9500ada2351e1c5c79fbda354b42492e185217f
Your Name <gopinathji1711@gmil.com>
2023-10-18 23:35:21 +0530

    node

---
+++ node js/1lessen.js
@@ -0,0 +1,11 @@
+function CalcuateArea(length,width)
+{
+    console.log("Calculate area function is called....");
+    console.log(length,width);
+    let area = length * width;
+    return area;
+}
+
+//call function 
+var area = CalcuateArea(100,200);
+console.log(area);
---
+++ node js/2exercise.js
@@ -0,0 +1,9 @@
+var http = require ("http");
+var my = require("./localmodul");
+var server = http.createServer(function(request,response){
+    console.log("I got your request.....");
+    console.log(my.getDate());
+    console.log(my.getTime());  
+});
+server.listen(5000);
+console.log("Server is running on port 5000");
